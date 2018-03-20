6025fa30981266a82d0440d638802703cefe3f81
Nico Stapelbroek <nstapelbroek@gmail.com>
2018-03-20 20:02:23 +0100

    get ALL stages and display them

--- src/__client__/stage/Stages.js
+++ src/__client__/stage/Stages.js
@@ -1,18 +1,51 @@
-import React, {Component} from 'react';
-import {ScrollView, View} from 'react-native';
+import React, { Component } from 'react';
+import { FlatList, ScrollView, View, Text } from 'react-native';
 import axios from 'axios';
-import {serverUrl} from '../config/server';
+import { serverUrl } from '../config/server';
 import StageOverviewCard from './StageOverviewCard';
 
-
-// TODO
-// get ALL stages and display them (Use the StageOverViewCard component to display one Stage)
-// Get all the stages: axios.get(`${serverUrl}/stages`);
-
 class Stages extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: [],
+    };
+  }
+
+  renderItem = ({ item }) => {
+    return <StageOverviewCard stage={item} />
+  };
+
+  renderSeparator = () => (
+    <View style={{ height: 1, backgroundColor: "grey", marginLeft: 80 }} />
+  );
+
+  renderHeader = () => (
+    <View
+      style={{ height: 30, backgroundColor: "#5E5EB4", justifyContent: "center", alignItems: 'center' }}
+    >
+      <Text style={{ color: 'white' }}> All Stages</Text>
+    </View>
+  );
+
+  async componentDidMount() {
+    const { data: stages } = await axios.get(`${serverUrl}/stages`);
+    this.setState({ stages });
+  }
 
   render() {
-    return null;
+    const { stages } = this.state;
+    console.log(stages);
+    return <View>
+      <FlatList
+        data={stages}
+        renderItem={this.renderItem}
+        ItemSeparatorComponent={this.renderSeparator}
+        ListHeaderComponent={this.renderHeader}
+        keyExtractor={item => item.id}
+      />
+    </View>
   }
 }
 
